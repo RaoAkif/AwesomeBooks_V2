86fac3bd7bdea0ca8189b4cd251dc28d544f3eed
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-04-03 21:31:41 +0500

    01_DOM_Display

---
+++ script.js
@@ -0,0 +1,40 @@
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
+
+const BookList = [
+  {
+    title: 'Book One',
+    author: 'Author One',
+    id: Math.random(),
+  },
+  {
+    title: 'Book Two',
+    author: 'Author Two',
+    id: Math.random(),
+  },
+  {
+    title: 'Book Three',
+    author: 'Author Three',
+    id: Math.random(),
+  },
+]
+
+const books = BookList;
+
+// Adding Books Rows
+const list = document.querySelector('#book-list-table');
+
+books.forEach((book) => {
+    list.innerHTML += `
+    <tr id=${book.id}>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td><button>Delete</a></button></td>
+    </tr>
+  `
+})
